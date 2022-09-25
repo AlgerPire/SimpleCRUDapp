83f14180f7d1de4c28941d963f05b79585097e5d
AlgerPire <58697445+AlgerPire@users.noreply.github.com>
2022-09-25 11:03:38 +0200

    Static Folder + style.css

--- src/main/java/com/example/person_account_adress/repositories/PersonRepository.java
+++ src/main/java/com/example/person_account_adress/repositories/PersonRepository.java
@@ -3,7 +3,9 @@ package com.example.person_account_adress.repositories;
 import com.example.person_account_adress.entities.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -11,4 +13,8 @@ import java.util.List;
 public interface PersonRepository extends JpaRepository<Person,Long> {
     @Query("Select p.name,p.age, p.email From Person p where p.age>18 ")
     List<Object> findAllAbove18Years();
+
+    @Query("Select p.name, p.email from  Person  p where p.name=:name")
+    List<Object> findAllWithGivenName(@Param("name") String name);
+
 }
--- src/main/java/com/example/person_account_adress/services/impl/PersonImpl.java
+++ src/main/java/com/example/person_account_adress/services/impl/PersonImpl.java
@@ -6,31 +6,37 @@ import com.example.person_account_adress.services.PersonService;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Service
-public class PersonImpl implements PersonService {
+// mund ti besh dhe implement PersonService dhe me pas ajo i ben override metodave per te arritur abstrakstionin
+public class PersonImpl {
 
     @Autowired
     private PersonRepository personRepository;
 
 
-    @Override
+
     public List<Person> listAll() {
         return personRepository.findAll();
     }
 
-    @Override
+
     public Person getPersonById(Long id) {
         return personRepository.findById(id).get();
     }
 
-    @Override
+
     public Person updatePerson(Long id, Person person) {
         Person person1=personRepository.findById(id).get();
         person1.setName(person.getName());
@@ -42,13 +48,13 @@ public class PersonImpl implements PersonService {
 
     }
 
-    @Override
+
     public void deletePerson(Long id) {
         personRepository.deleteById(id);
 
     }
 
-    @Override
+
     public Person addNewPerson(Person person) {
         personRepository.save(person);
         return person;
@@ -57,4 +63,24 @@ public class PersonImpl implements PersonService {
    public List<Object> findAllAbove18(){
         return personRepository.findAllAbove18Years();
    }
+
+   public List<Object> findWithGivenName(String name){
+        return personRepository.findAllWithGivenName(name);
+   }
+
+   private static final String GET_ALL_USERS_API="https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/search";
+
+    RestTemplate restTemplate=new RestTemplate();
+
+    public ResponseEntity<String> textRandomJokes(String query){
+        HttpHeaders headers=new HttpHeaders();
+        headers.setAccept(List.of(MediaType.APPLICATION_JSON));
+        headers.put("X-RapidAPI-Key", Collections.singletonList("3a9593658bmsh2ae43ba506ad25ep101582jsn762226628a7a"));
+        HttpEntity<String> entity=new HttpEntity<>("parameters",headers);
+        String linkTotal = GET_ALL_USERS_API + "?query=" + query;
+        return restTemplate.exchange(linkTotal, HttpMethod.GET,entity,String.class);
+
+    }
+
+
 }
